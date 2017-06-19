ea5fbcc03c3b471ed8f7ed5e9397d1f43b5d60c1
rodhuega <rodrigodenia@gmail.com>
2017-06-19 13:28:39 +0000

    diversos cambios

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,34 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.Initializable;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            Process p = Runtime.getRuntime().exec("msiklm test");
+            p.waitFor();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line=reader.readLine();
+            if(line.charAt(0)=='N') {
+                throw new IllegalArgumentException("Teclado no compatible");
+            }
+        }catch(IOException | InterruptedException | IllegalArgumentException e) {
+            System.out.println("Teclado no compatible");
+        }
+    }
+
+    public void makeChanges(){
+        try {
+            Process p = Runtime.getRuntime().exec("msiklm red");
+            //p.waitFor();
+        }catch(IOException e) {}
+    }
 }
